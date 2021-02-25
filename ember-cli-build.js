2df8c725dc5915cb2cbd0c2f4f3dcb7ce603bd52
Thomas Wang <wangxgwxg@gmail.com>
2021-02-25 12:25:30 -0800

    Enable embroider

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { Webpack } = require('@embroider/webpack');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
@@ -24,6 +25,10 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
-  return app.toTree();
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticComponents: true,
+  });
 };
