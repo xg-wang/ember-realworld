f6cde0d11d99108189f89f90a2a0bd9c48487430
Thomas Wang <wangxgwxg@gmail.com>
2021-02-25 12:08:07 -0800

    Upgrade marked, use esm version

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    autoImport: {
+      alias: {
+        marked: 'marked/lib/marked.esm.js',
+      },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
